d8e9b70b81804597658f728b27590b8fbcbbc093
Fabio José <fabiojose@gmail.com>
2019-07-30 20:26:32 -0300

    Import the endpoint files
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require("express");
+var auth = require("./auth.js");
+var employee = require("./employee.js");
+
 var app = express();
 
+// Serving statics
 app.use(express.static("ui"));
 
-app.listen(3000, function () {
+// Attaching the api routers
+app.use("/api", employee);
+app.use("/api/auth", auth);
+
+app.listen(3000, () => {
   console.log("Employee Manager listening on: http://localhost:3000");
 });
