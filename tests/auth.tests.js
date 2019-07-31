d28d83a0f60d75dcb1c0e7a37d6c059e4e44e0dc
Fabio José <fabiojose@gmail.com>
2019-07-31 17:02:03 -0300

    Tests for the logout
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- tests/auth.tests.js
+++ tests/auth.tests.js
@@ -3,6 +3,8 @@ const expect = require("chai").expect;
 
 const app = require("../app.js");
 
+var apitoken;
+
 describe("Authentication API", () => {
   describe("POST /api/login", () => {
     it("should response 200 code when credentials are ok", (done) => {
@@ -24,7 +26,20 @@ describe("Authentication API", () => {
         .expect(200)
         .expect((res) => {
           expect(res.body).to.have.property("apitoken");
+          apitoken = res.body.apitoken;
         })
+        .end(done);
+    });
+  });
+
+  describe("DELETE /api/login", () => {
+    it("should response 200 code when credentials are ok", (done) => {
+      // act & assert
+      supertest(app)
+        .delete("/api/login")
+        .set("x-apitoken", apitoken)
+        .set("Accept", "application/json")
+        .expect(204)
         .end(done);
     });
   });
