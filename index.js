31398dac4390e2789372400b0473143c0cde8810
Fabio José <fabiojose@gmail.com>
2019-07-30 22:45:23 -0300

    Just the listen()
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-var express = require("express");
-var auth = require("./auth.js");
-var employee = require("./employee.js");
-
-var app = express();
-
-// Serving statics
-app.use(express.static("ui"));
-
-// Attaching the api routers
-app.use("/api", employee);
-app.use("/api/auth", auth);
+const app = require("./app.js");
 
 app.listen(3000, () => {
-  console.log("Employee Manager listening on: http://localhost:3000");
+  console.log("> listening on: http://localhost:3000");
 });
